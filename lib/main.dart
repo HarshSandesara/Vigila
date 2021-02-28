3a22de2f6a1f40c43576a0d6622b9f13bad035e8
Harsh Sandesara <sandesara.harsh@gmail.com>
2021-02-28 12:05:20 +0530

    Draft

--- lib/main.dart
+++ lib/main.dart
@@ -201,18 +201,30 @@ class _MyNavigationBarState extends State<MyNavigationBar > {
       ListTile(
         leading: Icon(Icons.add_alert),
         title: Text('First Aid'),
+        onTap: () {
+          
+        },
       ),
       ListTile(
         leading: Icon(Icons.list),
         title: Text('RICE'),
+        onTap: () {
+          
+        },
       ),
       ListTile(
         leading: Icon(Icons.favorite),
         title: Text('CPR'),
+        onTap: () {
+          
+        },
         ),
       ListTile(
         leading: Icon(Icons.list_alt),
         title: Text('BLS'),
+        onTap: () {
+          
+        },
         ),
       ],
       ),  
@@ -230,7 +242,7 @@ class _MyNavigationBarState extends State<MyNavigationBar > {
           ),
         ),
     ),
-    Text('Profile Page', style: TextStyle(fontSize: 25, fontWeight: FontWeight.normal)),
+    ProfileUI2(),
   ];
   
   void _onItemTapped(int index) {
@@ -278,3 +290,114 @@ class _MyNavigationBarState extends State<MyNavigationBar > {
     );
   }  
 }  
+
+class ProfileUI2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(
+                    "add you image URL here "
+                  ),
+                  fit: BoxFit.cover
+                )
+              ),
+            child: Container(
+              width: double.infinity,
+              height: 200,
+              child: Container(
+                alignment: Alignment(0.0,2.5),
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://cdn.iconscout.com/icon/free/png-256/avatar-370-456322.png"
+                  ),
+                  radius: 60.0,
+                ),
+              ),
+            ),
+            ),
+
+            SizedBox(
+              height: 60,
+            ),
+            Text(
+              "Aschen Jays (pronounced Hays)",
+              style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.blueGrey,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w400
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Mumbai, India"
+              ,style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              "Phone Number: 1234567890"
+              ,style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            Text(
+              "Email ID: aschen.jays@gmail.com",
+                style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            Text(
+              "Address: My address",
+                style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            Text(
+              "Blood Group: O+",
+                style: TextStyle(
+                fontSize: 18.0,
+                color:Colors.black45,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.w300
+            ),
+            ),
+            SizedBox(
+              height: 50,
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
