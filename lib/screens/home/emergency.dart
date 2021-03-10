66864cd9c858063df57eb71f35802a5ce1544b05
Rohit Pai <rohit23110@gmail.com>
2021-03-10 08:34:24 +0530

    Added remaining files missed out in previous commit

--- lib/screens/home/emergency.dart
+++ lib/screens/home/emergency.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:vigila/models/user.dart';
+import 'package:vigila/services/auth.dart';
 
 class Emergency extends StatelessWidget {
+  // final AuthService _auth = AuthService();
+  // CustomUser user;
+
+  // _getCustomUser() {
+  //   _auth.user.listen((customUser) {
+
+  //   });
+  // }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,63 +33,60 @@ class Emergency extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: ListView(
+              child: ListView(
             children: <Widget>[
               Card(
                 child: ListTile(
-                  leading: Icon(Icons.person,
-                  size: 40),
+                  leading: Icon(Icons.person, size: 40),
                   title: Text('Harsh Sandesara'),
-                  subtitle: Text('6942069420',
-                  style: TextStyle(
-                    color: Colors.purple,
-                  ),),
+                  subtitle: Text(
+                    '6942069420',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),
+                  ),
                   trailing: Icon(Icons.call),
-                  onTap: () {
-
-                  },
+                  onTap: () {},
                 ),
               ),
               Card(
                 child: ListTile(
-                  leading: Icon(Icons.person,
-                  size: 40),
+                  leading: Icon(Icons.person, size: 40),
                   title: Text('Rohit Pai'),
-                  subtitle: Text('0123456789',
-                  style: TextStyle(
-                    color: Colors.purple,
-                  ),),
+                  subtitle: Text(
+                    '0123456789',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),
+                  ),
                   trailing: Icon(Icons.call),
-                  onTap: () {
-                    
-                  },
+                  onTap: () {},
                 ),
               ),
               Card(
                 child: ListTile(
-                  leading: Icon(Icons.person,
-                  size: 40),
+                  leading: Icon(Icons.person, size: 40),
                   title: Text('Ashutosh Naik'),
-                  subtitle: Text('9876543210',
-                  style: TextStyle(
-                    color: Colors.purple,
-                  ),),
+                  subtitle: Text(
+                    '9876543210',
+                    style: TextStyle(
+                      color: Colors.purple,
+                    ),
+                  ),
                   trailing: Icon(Icons.call),
-                  onTap: () {
-                    
-                  },
+                  onTap: () {},
                 ),
               ),
             ],
           )),
           Center(
-            child: FlatButton(onPressed: () {
-
-            }, child: Text('Add Contact'),
-            color: Colors.purple.shade400,
-            textColor: Colors.white,
-            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(5)))
-            ),
+            child: FlatButton(
+                onPressed: () {},
+                child: Text('Add Contact'),
+                color: Colors.purple.shade400,
+                textColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(5)))),
           ),
         ],
       ),
